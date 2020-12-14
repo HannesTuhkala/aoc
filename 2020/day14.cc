9821d0588f3da6654cee53833f204d2e3e3ec062
Hannes Tuhkala <hantu447@student.liu.se>
2020-12-14 18:59:30 +0100

    .

--- 2020/day14.cc
+++ 2020/day14.cc
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <vector>
 #include <bitset>
 
-std::string apply_mask(unsigned long long value, std::string& mask) {
+std::string apply_mask_a(unsigned long long value, std::string& mask) {
 	std::string string_value = std::bitset<36>(value).to_string();
 
 	for (int i{0}; i < 36; i++) {
@@ -13,6 +14,23 @@ std::string apply_mask(unsigned long long value, std::string& mask) {
 	}
 
 	return string_value;
+}
+
+std::vector<int> apply_mask_b(int address, std::string& mask) {
+	std::string string_address = std::bitset<36>(address).to_string();
+	std::vector<int> addresses{};
+
+	for (int i{0}; i < 36; i++) {
+		if (mask[i] == 0) {
+			continue;
+		} else if (mask[i] == 1) {
+			string_address[i] = 1;
+		} else {
+			string_address[i] = 'X';
+		}
+	}
+
+	return addresses;
 }
 
 int main() {
@@ -29,7 +47,7 @@ int main() {
 		} else {
 			address = stoi(tempLine.substr(4, tempLine.find(']', 4) - 4));
 			value = stoi(tempLine.substr(tempLine.find("= ", 6) + 2, std::string::npos));
-			memory[address] = apply_mask(value, mask);
+			memory[address] = apply_mask_a(value, mask);
 		}
 	}
 
@@ -37,6 +55,9 @@ int main() {
 	for (auto&& pair : memory) {
 		sum += std::bitset<36>(pair.second).to_ullong();
 	}
+
+	source.clear();
+	source.seekg(0, std::ios::beg);
 
 	std::cout << "The answer to part a) is: " << sum << std::endl;
 }
